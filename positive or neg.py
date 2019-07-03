28875b7363044d84f0b6313067c4b8702499ed05
raghava430 <42106004+raghava430@users.noreply.github.com>
2019-07-03 10:06:18 +0530

    Add files via upload

--- positive or neg.py
+++ positive or neg.py
@@ -1,7 +1,7 @@
-num = float(input())
+num = float(input("Enter a number: "))
 if num > 0:
-   print("Positive")
+   print("Positive number")
 elif num == 0:
    print("Zero")
 else:
-    print("Negative")
+    print("Negative number")
