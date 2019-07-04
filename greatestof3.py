deafbf2c4ebdf3025aeff0a0566d29851f9e1d89
raghava430 <42106004+raghava430@users.noreply.github.com>
2019-07-04 10:07:13 +0530

    Update greatestof3.py

--- greatestof3.py
+++ greatestof3.py
@@ -1,10 +1,8 @@
-a=float(input())
-b=float(input())
-c=float(input())
-if ( a>b and a>c ):
-    print("a is greater")
-elif(b>a and b>c):
-    print("b is greater")
-elif(c>a and c>b):
-     print("c is greater")
+a,b,c = map(int,input().split())
+if a>b and a>c:
+    print(a)
+    elif b>c:
+        print(b)
+        else:
+            print(c)
             
