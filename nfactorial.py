9664e4b9aaceeb3b220f23bbcd5c481be9ada65d
raghava430 <42106004+raghava430@users.noreply.github.com>
2019-07-05 10:14:04 +0530

    Update nfactorial.py

--- nfactorial.py
+++ nfactorial.py
@@ -1,9 +1,9 @@
-i=int(input())
-if i==0:
-    sk=1
-    print(sk)
-elif i>0:
+k=int(input())
+if k==0:
+    gh=1
+    print(gh)
+elif k>0:
     sum=1
-    for i in range(1,i+1):
-        sum=sum*i
+    for k in range(1,k+1):
+        sum=sum*k
     print(sum)
